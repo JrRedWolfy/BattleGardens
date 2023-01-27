f3f7b9ce0fc19848377f9d54dfc8a83c4e614906
David <RedCatter554@gmail.com>
2023-01-27 14:27:03 +0100

    avances en sql

--- Base de Datos/bbdd-v2.sql
+++ Base de Datos/bbdd-v2.sql
@@ -7,24 +7,28 @@ clave varchar(256) not null
 
 create table artefacto(
 id_artefacto int auto_increment,
-nombre varchar(30),
-nickname varchar(24),
+nombre varchar(30) not null,
+imagen_artefacto varchar(200) not null,
+autor varchar(24) not null,
 
-CONSTRAINT FK17 FOREIGN KEY(nickname)  REFERENCES usuario (nickname) ON UPDATE CASCADE ON DELETE RESTRICT 
+CONSTRAINT FK17 FOREIGN KEY(autor)  REFERENCES usuario (nickname) ON UPDATE CASCADE ON DELETE RESTRICT 
 );
 
 create table evento(
 id_evento int primary key,
 contenido varchar(300),
+autor varchar(24) not null,
 
-CONSTRAINT FK17 FOREIGN KEY(nickname)  REFERENCES usuario (nickname) ON UPDATE CASCADE ON DELETE RESTRICT 
+CONSTRAINT FK17 FOREIGN KEY(autor)  REFERENCES usuario (nickname) ON UPDATE CASCADE ON DELETE RESTRICT 
 );
 
 create table publicacion(
 id_publicacion int primary key,
-conte
+contenido varchar(400) not null,
+imagen varchar(200),
+autor varchar(24) not null,
 
-CONSTRAINT FK17 FOREIGN KEY(nickname)  REFERENCES usuario (nickname) ON UPDATE CASCADE ON DELETE RESTRICT   
+CONSTRAINT FK17 FOREIGN KEY(autor)  REFERENCES usuario (nickname) ON UPDATE CASCADE ON DELETE RESTRICT   
 );
 
 create table preferencias(
@@ -33,8 +37,12 @@ create table preferencias(
 );
 
 create table historia(
+id_historia int primary key,
+titulo varchar(30) not null,
+contenido varchar(500) not null,
+autor varchar(24) not null,
 
-CONSTRAINT FK17 FOREIGN KEY(nickname)  REFERENCES usuario (nickname) ON UPDATE CASCADE ON DELETE RESTRICT     
+CONSTRAINT FK17 FOREIGN KEY(autor)  REFERENCES usuario (nickname) ON UPDATE CASCADE ON DELETE RESTRICT     
 );
 
 create table relacciones(
